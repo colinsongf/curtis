b8060b06fbeaf96252ad13afc9c5859b3dfc1951
Ryan Sloan <ryan@ryanjsloan.com>
2014-09-13 18:11:29 -0700

    fixing static files according to heroku docs

--- curtis/settings.py
+++ curtis/settings.py
@@ -81,6 +81,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/curtis/static/'
-
+STATIC_ROOT = 'staticfiles'
 TEMPLATE_DIRS = ( os.path.dirname(__file__),os.path.dirname(__file__)+"/curtis",)
-STATICFILES_DIRS = ("/Users/ryansloan/Desktop/Code/html/curtis/curtis/static/",)
+STATICFILES_DIRS = (os.path.dirname(__file__)+"/static/",)
